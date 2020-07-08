9e471d077f71d57f8bd1cd599bd597c87d4c5086
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-07-08 13:50:03 -0400

    adding

---
+++ week-1/profile-app/src/app/app.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: app.component.ts
+ * Author: Professor Krasso
+ * Date: 8 July 2020
+ * Modified by: Diandra McKenzie
+ * Description: App component
+ */
+
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  assignment: string = 'Assignment 1.5 - Components';
+}
+
+
+
+
---
+++ week-1/profile-app/src/app/my-details/my-details.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: my-details.component.ts
+ * Author: Professor Krasso
+ * Date: 8 July 2020
+ * Modified By" Diandra McKenzie
+ * Description: My details component (Details view)
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-details',
+  templateUrl: './my-details.component.html',
+  styleUrls: ['./my-details.component.css']
+})
+export class MyDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
