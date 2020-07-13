e794bffb348b4317b3f1a28e9c29ef8625c18a96
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-07-13 19:22:14 -0400

    adding

---
+++ week-2/my-app/src/app/about/about.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: about.component.ts
+ * Author: Professor Krasso
+ * Date: 13 July 2020
+ * Modified by: Diandra McKenzie
+ * Description: About component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ week-2/my-app/src/app/app-routing.module.ts
@@ -0,0 +1,39 @@
+/**
+ * Title: app-routing.component.ts
+ * Author: Professor Krasso
+ * Date: 13 July 2020
+ * Modified by: Diandra McKenzie
+ * Description: Routing Module
+ */
+
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ContactComponent } from './contact/contact.component';
+import { AboutComponent } from './about/about.component';
+
+
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'contact',
+    component: ContactComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
