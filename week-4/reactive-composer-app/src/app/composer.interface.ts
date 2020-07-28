e7bde27e118ee8b853eb8a53939f04e561f7d76c
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-07-27 22:50:40 -0400

    adding

---
+++ week-4/reactive-composer-app/src/app/composer.interface.ts
@@ -0,0 +1,13 @@
+/**
+ * Title: composer.interface.ts
+ * Author: Professor Krasso
+ * Date: 21 July 2020
+ * Modified by: Diandra McKenzie
+ * Description: Composer Interface
+ */
+
+export interface IComposer {
+  composerId: number;
+  fullName: string;
+  genre: string;
+}
---
+++ week-4/reactive-composer-app/src/app/composer.service.ts
@@ -0,0 +1,40 @@
+/**
+ * Title: composer.service.ts
+ * Author: Professor Krasso
+ * Date: 27 July 2020
+ * Modified by: Diandra McKenzie
+ * Description: Composer Service File
+ */
+
+import { Injectable } from '@angular/core';
+import { IComposer } from './composer.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ComposerService {
+
+  composers: Array<IComposer>;
+
+  constructor () {
+    this.composers = [
+      {composerId: 1, fullName: "Duke Ellington", genre: "Jazz"},
+      {composerId: 2, fullName: "Miles Davis", genre: "Jazz"},
+      {composerId: 3, fullName: "John Coltrane", genre: "Jazz"},
+      {composerId: 4, fullName: "Thelonious Monk", genre: "Jazz"},
+      {composerId: 5, fullName: "Robert Glasper", genre: "Jazz"}
+    ]
+  }
+
+  getComposers() {
+    return this.composers;
+  }
+
+  getComposer(composerId: number) {
+    for (let composer of this.composers) {
+      if (composer.composerId === composerId) {
+        return composer;
+      }
+    }
+  }
+}
