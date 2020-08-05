ba498cb9dbb4e5a087bb2d9ba5b2719f034b0d63
Diandra McKenzie <diandramckenzie79@gmail.com>
2020-08-04 21:43:53 -0400

    adding

---
+++ week-5/in-n-out-booksp1/src/app/book-list/book-list.component.ts
@@ -0,0 +1,23 @@
+/**
+ * Title: book-list.component.ts
+ * Author: Professor Krasso
+ * Date: 4 August 2020
+ * Modified By: Diandra McKenzie
+ * Description: Book-list component
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
